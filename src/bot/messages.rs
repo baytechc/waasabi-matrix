4f09c951186620460fca9836f9c76823da04b912
Jan-Erik Rediger <janerik@fnordig.de>
2020-10-17 18:19:38 +0200

    Split out fetching joined rooms

--- src/bot/messages.rs
+++ src/bot/messages.rs
@@ -1,46 +1,30 @@
-use std::convert::TryFrom;
 use crate::matrix;
 
-use ruma::{
-    api::client::r0::{
-        membership::{
-            invite_user::{self, InvitationRecipient},
-            joined_rooms,
-        },
-    },
-    UserId,
-    RoomId,
-};
+use ruma::{RoomId, UserId};
 use ruma_client::{self, HttpsClient};
 
-pub async fn handle(client: &HttpsClient, room_id: &RoomId, sender: &UserId, msg: &str) -> anyhow::Result<()> {
+pub async fn handle(
+    client: &HttpsClient,
+    room_id: &RoomId,
+    sender: &UserId,
+    msg: &str,
+) -> anyhow::Result<()> {
     println!("{:?} in {:?}: {}", sender, room_id, msg);
 
     if sender == "@jer:rustch.at" {
         if msg == "!channels" {
             println!("channel listing request from Jan-Erik in #rustfest-test");
-            let response = client.request(joined_rooms::Request::new()).await?;
-
-            let rooms = response
-                .joined_rooms
-                .into_iter()
-                .map(|room| room.as_str().to_string())
-                .collect::<Vec<_>>();
+            let rooms = matrix::joined_rooms(client).await?;
             let msg = rooms.join(", ");
-
             matrix::send_message(&client, &room_id, msg).await?;
         }
 
         if msg.starts_with("!invite ") {
             let mut parts = msg.split(" ");
             let name = parts.nth(1).unwrap();
-            let user_id = UserId::try_from(name).unwrap();
-            let recipient = InvitationRecipient::UserId { user_id: &user_id };
             println!("Inviting {} to {}", name, room_id);
             if !name.is_empty() {
-                client
-                    .request(invite_user::Request::new(&room_id, recipient))
-                    .await?;
+                matrix::invite_user(client, &room_id, name).await?
             }
         }
     }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,13 +1,51 @@
-use std::convert::TryFrom;
+use std::{
+    convert::TryFrom,
+    sync::atomic::{AtomicUsize, Ordering},
+};
 
 use ruma::{
     api::client::r0::{
         alias::get_alias,
-        membership::invite_user::{self, InvitationRecipient},
+        membership::{
+            invite_user::{self, InvitationRecipient},
+            joined_rooms,
+        },
+        message::send_message_event,
+    },
+    events::{
+        room::message::{MessageEventContent, TextMessageEventContent},
+        AnyMessageEventContent,
     },
     RoomAliasId, RoomId, UserId,
 };
 use ruma_client::{self, HttpsClient};
+
+/// Monotonically increasing counter
+fn next_id() -> String {
+    static NEXT_ID: AtomicUsize = AtomicUsize::new(1);
+    NEXT_ID.fetch_add(1, Ordering::SeqCst).to_string()
+}
+
+pub async fn send_message<S: Into<String>>(
+    matrix_client: &HttpsClient,
+    room_id: &RoomId,
+    msg: S,
+) -> anyhow::Result<()> {
+    matrix_client
+        .request(send_message_event::Request::new(
+            &room_id,
+            &next_id(),
+            &AnyMessageEventContent::RoomMessage(MessageEventContent::Text(
+                TextMessageEventContent {
+                    body: msg.into(),
+                    formatted: None,
+                    relates_to: None,
+                },
+            )),
+        ))
+        .await?;
+    Ok(())
+}
 
 pub async fn real_room_id(
     matrix_client: &HttpsClient,
@@ -34,4 +72,15 @@ pub async fn invite_user(
         .await?;
 
     Ok(())
+}
+
+pub async fn joined_rooms(matrix_client: &HttpsClient) -> anyhow::Result<Vec<String>> {
+    let response = matrix_client.request(joined_rooms::Request::new()).await?;
+
+    let rooms = response
+        .joined_rooms
+        .into_iter()
+        .map(|room| room.as_str().to_string())
+        .collect::<Vec<_>>();
+    Ok(rooms)
 }
