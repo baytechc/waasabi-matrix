38dc9525bfe13439b328e36031d75bfb9f5b421f
Jan-Erik Rediger <janerik@fnordig.de>
2020-10-17 18:14:24 +0200

    Split out message handling

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -1,6 +1,4 @@
 use std::{
-    convert::TryFrom,
-    sync::atomic::{AtomicUsize, Ordering},
     time::Duration,
 };
 
@@ -10,25 +8,19 @@ use ruma::{
     api::client::r0::{
         filter::FilterDefinition,
         membership::{
-            invite_user::{self, InvitationRecipient},
-            join_room_by_id, joined_rooms,
+            join_room_by_id,
         },
-        message::send_message_event,
         sync::sync_events,
     },
     events::{
         room::message::{MessageEventContent, TextMessageEventContent},
-        AnyMessageEventContent, AnySyncMessageEvent, AnySyncRoomEvent, SyncMessageEvent,
+        AnySyncMessageEvent, AnySyncRoomEvent, SyncMessageEvent,
     },
     presence::PresenceState,
-    UserId,
 };
 use ruma_client::{self, HttpsClient};
 
-fn next_id() -> String {
-    static NEXT_ID: AtomicUsize = AtomicUsize::new(1);
-    NEXT_ID.fetch_add(1, Ordering::SeqCst).to_string()
-}
+mod messages;
 
 pub async fn event_loop(client: HttpsClient) -> anyhow::Result<()> {
     let initial_sync_response = client
@@ -72,48 +64,7 @@ pub async fn event_loop(client: HttpsClient) -> anyhow::Result<()> {
                     },
                 )) = event
                 {
-                    println!("{:?} in {:?}: {}", sender, room_id, msg_body);
-
-                    if sender == "@jer:rustch.at" {
-                        if msg_body == "!channels" {
-                            println!("channel listing request from Jan-Erik in #rustfest-test");
-                            let response = client.request(joined_rooms::Request::new()).await?;
-
-                            let rooms = response
-                                .joined_rooms
-                                .into_iter()
-                                .map(|room| room.as_str().to_string())
-                                .collect::<Vec<_>>();
-                            let msg = rooms.join(", ");
-
-                            client
-                                .request(send_message_event::Request::new(
-                                    &room_id,
-                                    &next_id(),
-                                    &AnyMessageEventContent::RoomMessage(
-                                        MessageEventContent::Text(TextMessageEventContent {
-                                            body: msg.into(),
-                                            formatted: None,
-                                            relates_to: None,
-                                        }),
-                                    ),
-                                ))
-                                .await?;
-                        }
-
-                        if msg_body.starts_with("!invite ") {
-                            let mut parts = msg_body.split(" ");
-                            let name = parts.nth(1).unwrap();
-                            let user_id = UserId::try_from(name).unwrap();
-                            let recipient = InvitationRecipient::UserId { user_id: &user_id };
-                            println!("Inviting {} to {}", name, room_id);
-                            if !name.is_empty() {
-                                client
-                                    .request(invite_user::Request::new(&room_id, recipient))
-                                    .await?;
-                            }
-                        }
-                    }
+                    messages::handle(&client, &room_id, &sender, &msg_body).await?;
                 }
             }
         }
