14793efb492977bba44ea93c7d965b91599f9253
Jan-Erik Rediger <janerik@fnordig.de>
2020-10-17 23:48:27 +0200

    Add a strapi client

---
+++ src/strapi.rs
@@ -0,0 +1,59 @@
+use anyhow::bail;
+use reqwest::StatusCode;
+use serde::{Deserialize, Serialize};
+
+const BACKEND_BASE: &str = "https://backend.rustfest.global";
+
+pub struct Client {
+    http: reqwest::Client,
+    jwt: String,
+}
+
+#[derive(Serialize)]
+struct Login<'a> {
+    identifier: &'a str,
+    password: &'a str,
+}
+
+#[derive(Deserialize)]
+struct LoginResponse {
+    jwt: String,
+}
+
+fn url(path: &str) -> String {
+    format!("{}/{}", BACKEND_BASE, path)
+}
+
+pub async fn login(identifier: &str, password: &str) -> anyhow::Result<Client> {
+    let http = reqwest::Client::builder()
+        .user_agent("ferris-bot/0.1.0")
+        .build()?;
+
+    let login = Login { identifier, password };
+    let response = http.post(&url("auth/local")).json(&login).send().await?;
+    if response.status() != StatusCode::OK {
+        bail!("Failed to login");
+    }
+
+    let response: LoginResponse = response.json().await?;
+    let jwt = response.jwt;
+    Ok(Client { http, jwt })
+}
+
+pub async fn post<T: Serialize + ?Sized>(
+    client: &Client,
+    path: &str,
+    data: &T,
+) -> anyhow::Result<()> {
+    log::debug!("JWT: {}", client.jwt);
+    let res = client
+        .http
+        .post(&url(path))
+        .bearer_auth(&client.jwt)
+        .json(data)
+        .send()
+        .await?;
+    log::debug!("Response: {:?}", res);
+
+    Ok(())
+}
